3244d786d4875ac4c2e1155dda4fc2e38d68dbe5
Chino <ronaldcubillo92@gmail.com>
2019-02-22 11:24:11 -0600

    Working on the HTML and the button start

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+function restart (){
+
+
+}
+
+
+$("#startbtn").on("click", function() {
+    var questions = "<div> This is a test <div>"
+    $("#triviaQuestions").text(questions);
+
+    ("#startbtn").style.visibility = "hidden";
+  });
