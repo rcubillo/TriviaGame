95c57d550859f6810dcb553f71234c20eefa1bfc
Chino <ronaldcubillo92@gmail.com>
2019-02-24 19:48:15 -0600

    Adding a timer, form with questions, and a button done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,70 @@
-function restart (){
+//Variables
+var intervalId;
+var number = 15;
 
+//Hidden the form when the page loads.
+$("form").addClass("hidden");
+
+
+function restart() {
+  $("#startbtn").addClass("hidden");
+}
+
+function timer() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+  //  Decrease number by one.
+  number--;
+  //  Show the number in the #show-number tag.
+  $("#time-left").html("<h2>" + number + "</h2>");
+  //  Once number hits zero...
+  if (number === 0) {
+    //  ...run the stop function.
+    stop();
+    //  Alert the user that time is up.
+    alert("Time Up!");
+  }
+}
+
+
+function capturingvalues() {
+  var x = document.getElementById("myRadio").value;
+  document.getElementById("demo").innerHTML = x;
+  console.log();
 
 }
 
 
-$("#startbtn").on("click", function() {
-    var questions = "<div> This is a test <div>"
-    $("#triviaQuestions").text(questions);
+//  The stop function
+function stop() {
 
-    ("#startbtn").style.visibility = "hidden";
-  });
+  //  Clears our intervalId
+  //  We just pass the name of the interval
+  //  to the clearInterval function.
+  clearInterval(intervalId);
+}
+
+function InsertingQuestions() {
+
+}
+
+$("#donebtn").on("click", function () {
+  capturingvalues();
+});
+
+$("#startbtn").on("click", function () {
+  $("form").show();
+  //a.setAttribute("hidden");
+  //$( "a" ).hide();
+  timer();
+  restart();
+});
+
+
+
+
+
+
