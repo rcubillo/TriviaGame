c52d5c45ff04cfe878aef0d45015188653a97649
Chino <ronaldcubillo92@gmail.com>
2019-02-25 10:15:47 -0600

    .checked() working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,15 @@
 //Variables
 var intervalId;
 var number = 15;
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
 
 //Hidden the form when the page loads.
 $("form").addClass("hidden");
 
 
+//Timer
 function restart() {
   $("#startbtn").addClass("hidden");
 }
@@ -29,13 +33,35 @@ function decrement() {
   }
 }
 
+$("input:checkbox").on('click', function() {
+  // in the handler, 'this' refers to the box clicked on
+  var $box = $(this);
+  if ($box.is(":checked")) {
+    // the name of the box is retrieved using the .attr() method
+    // as it is assumed and expected to be immutable
+    var group = "input:checkbox[name='" + $box.attr("name") + "']";
+    // the checked state of the group/box on the other hand will change
+    // and the current value is retrieved using .prop() method
+    $(group).prop("checked", false);
+    $box.prop("checked", true);
+  } else {
+    $box.prop("checked", false);
+  }
+});
 
-function capturingvalues() {
-  var x = document.getElementById("myRadio").value;
-  document.getElementById("demo").innerHTML = x;
-  console.log();
+$( "input" ).on( "click", function() {
+  $( "#log" ).html( $( "input:checked" ).val() + " is checked!" );
 
-}
+  if(document.getElementById('myCheck1').checked) {
+    console.log("you are the bomb");
+  }else if(document.getElementById('myCheck2').checked) {
+    console.log("you lost");
+  }
+  else if(document.getElementById('myCheck3').checked){
+    console.log("you lost");
+  }
+  
+});
 
 
 //  The stop function
