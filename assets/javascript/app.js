0f1acbc340b9368695da60e1ae1943c8c61ad9e5
Chino <ronaldcubillo92@gmail.com>
2019-02-28 16:50:49 -0600

    new functions, new design

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,145 +1,166 @@
+//this will ensure that jQuery functions won't fail due to unloaded pages.
 $(document).ready(function () {
+  $("#answerScreen").hide();
+  $(".container").hide();
+  //array with the questions and answers
+  var triviaQuestions = [
+      {
+        question: "Name the largest freshwater lake in the world?",
+        answers: ["Lake Superior", "Lake Tahoe", "Lake  Powel"],
+        name: "freshwater",
+        class: ".freshwater",
+        correct: "Lake Superior"
+      },
+      {
+        question: "What does CSS stands for?",
+        answers: ['Cascading Style Sheet', 'Compress  Style Sheet', 'Compact Solid Source'],
+        name: "css",
+        class: ".css",
+        correct: 'Cascading Style Sheet'
+      },
+      {
+        question: "What is the seventh planet from the sun?",
+        answers: ['Uranus', 'Mercury', 'Neptune'],
+        name: "planet",
+        class: ".planet",
+        correct: 'Uranus'
+      },
+      {
+        question: "Name the world's largest ocean?",
+        answers: ['Atlantic', 'Pacific', 'Indian'],
+        name: "world",
+        class: ".world",
+        correct: 'Pacific'
+      },
+      {
+        question: "What is the diameter of Earth?",
+        answers: ['50,000 miles', '24,000 miles', '8,000 miles'],
+        name: "diameter",
+        class: ".diameter",
+        correct: '8,000 miles'
+      },
+      {
+        question: "What colour jersey is worn by the winners of each stage of the Tour De France?",
+        answers: ['Yellow', 'Pink', 'Purple'],
+        name: "jersey",
+        class: ".jersey",
+        correct: 'Yellow'
+      },
+      {
+        question: "Which sport does Constantino Rocca play?",
+        answers: ['Soccer', 'Golf', 'Volleyball'],
+        name: "sport",
+        class: ".sport",
+        correct: 'Golf'
+      },
+      {
+        question: "Which chess piece can only move diagonally?",
+        answers: ['A  pawn', 'A knight', 'A bishop'],
+        name: "chess",
+        class: ".chess",
+        correct: 'A bishop'
+      }
+    ]
+  var labels = ["a", "b", "c", "d"];
+  
+   //  Set our number counter to 100.
+   var number = 100;
 
-  //Variables
-  var intervalId;
-  var number = 5;
-  var correct = 0;
-  var incorrect = 0;
-  var selected = [];
-  var rightanswers = ["#myCheck1,#myCheck4,#myCheck7,#myCheck11,#myCheck15,#myCheck16,#myCheck20,#myCheck24,#myCheck25"]
-  var incorrectanswers = ["#myCheck2", "#myCheck2", "#myCheck5", "#myCheck6", "#myCheck8", "#myCheck9", "#myCheck10", "#myCheck12", "#myCheck13", "#myCheck14", "#myCheck17", "#myCheck18", "#myCheck19", "#myCheck21", "#myCheck22", "#myCheck23", "#myCheck26", "#myCheck27"];
-  var unanswered = 0;
-  var correctanswers =
-
-    //Hidden the form and div when the page loads.
-  $("form").addClass("hidden");
-  $(".results").addClass("hidden");
-
-
-  //Timer
-  function timer() {
+  //  Variable that will hold our interval ID when we execute
+  //  the "run" function
+   var intervalId;
+  
+  // click to start then display quesions
+  var startGame = $("#start-btn").on('click', function() {
+  $(this).parent().hide();
+  $('.container').show();
+  $('#answerScreen').hide();
+  $('#timesUp').hide();
+  //  Execute the run function.
+  run();
+  questionDisplay();
+  });
+  
+  // function for displaying triviaQuestions
+  var questionDisplay = function() {
+  $(".questions :not('#sub-but')").empty();
+  // loops through the 8 triviaquestions
+  for (var j = 0; j < 8; j++) {
+  $('.questions').prepend('<div class="' + triviaQuestions[j].name + '"></div>');
+  $(triviaQuestions[j].class).append('<div class ="ques-title">' + triviaQuestions[j].question + '</div>');
+  // loops through answers for each radio button
+  for (var i = 0; i <= 2; i++) {
+    $(triviaQuestions[j].class).append('<input type="radio"  name="' + triviaQuestions[j].name + '" value="' + triviaQuestions[j].answers[i] + '"/><label for="' + labels[i] + '">' + triviaQuestions[j].answers[i] + '</label>');
+  }
+  $('.questions').prepend('<hr/>');
+  }
+  }
+  
+  function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
   }
-
-  //  The stop function
-  function stop() {
-    //  Clears the intervalId
-    clearInterval(intervalId);
-  }
-
-  function decrement() {
+   //  The decrement function.
+   function decrement() {
     //  Decrease number by one.
     number--;
-    //  Show the number in the #time-left tag.
-    $("#time-left").html("<h2>" + number + "</h2>");
+    //  Show the number in the #show-number tag.
+    $("#time-remain").html("<h5>" + number + "</h5>");
     //  Once number hits zero...
     if (number === 0) {
       //  ...run the stop function.
       stop();
       //  Alert the user that time is up.
       alert("Time Up!");
-      $("form").hide();
-      $(".results").show();
-      counting();
-      $
     }
   }
 
-  function restart() {
-    number = 5;
-    correct = 0;
-    incorrect = 0;
-    $("form").show();
-    $(".results").addClass("hidden");
-    $(".results").hide();
-    timer();
+  // stop function
+  function stop() {
+    //  to the clearInterval function.
+    clearInterval(intervalId);
   }
 
-  $('.question input:checked').each(function () {
-    selected.push($(this).attr('name'));
-  });
   
-
-  //Allowing to choose only one checkbox.
-  $("input:checkbox").on('click', function () {
-    // in the handler, 'this' refers to the box clicked on
-    var $box = $(this);
-    if ($box.is(":checked")) {
-      // the name of the box is retrieved using the .attr() method
-      // as it is assumed and expected to be immutable
-      var group = "input:checkbox[name='" + $box.attr("name") + "']";
-      // the checked state of the group/box on the other hand will change
-      // and the current value is retrieved using .prop() method
-      $(group).prop("checked", false);
-      $box.prop("checked", true);
-      console.log(group);
-    } else {
-      $box.prop("checked", false);
+  var gradeQuiz = $('#sub-but').on('click', function() {
+    var correctAnswers = 0;
+    var wrongAnswers = 0;
+    var unAnswered = 0;
+   // loop through correctArray & radioName to match html elements & answers
+   for (var i = 0; i < 8; i++) {
+    if ($('input:radio[name="' + triviaQuestions[i].name + '"]:checked').val() === triviaQuestions[i].correct) {
+        correctAnswers++;
+    } 
+    else if($("input:radio:not(:checked)")){
+  unAnswered++;
     }
-  });
+    else {
+      wrongAnswers++;
+  }};
 
-  $("input").one("click", function () {
-    //var correctopt = document.querySelectorAll("#myCheck1, #myCheck4").checked;
-    //var wrongopt = document.querySelectorAll("#myCheck2, #myCheck4, #myCheck3, #myCheck5, #myCheck6");
-    // if (document.querySelectorAll("#myCheck1, #myCheck4").checked) {
-    //   correct++;
-    //   console.log("correct " + correct);
-    // }
+  stop();
 
-    // else if (document.querySelectorAll("#myCheck2, #myCheck4, #myCheck3, #myCheck5, #myCheck6")){
-    //   incorrect++;
-    //   console.log("incorrect " + incorrect);
-    // }
+  $('#correctScreen').append(correctAnswers);
+  $('#wrongScreen').append(wrongAnswers);
+  $("#unaswered").append(unAnswered);
+  $('#answerScreen').show();
+  $('.container').hide();
+  // display correctAnswers
+  
+  }); // end gradeQuiz
 
-    //$("#log").html($("input:checked").val() + " is checked!");
-    // if (document.getElementById('myCheck1').checked) {
-    //   correct++;
-    //   console.log("correct " + correct);
-    // } else if (document.getElementById('myCheck2').checked) {
-    //   incorrect++;
-    //   console.log("incorrect " + incorrect);
-    // }
-    // else if (document.getElementById('myCheck3').checked) {
-    //   incorrect++;
-    //   console.log("incorrect " + incorrect);
-    // }
-    // console.log(correct);
-    // console.log(incorrect);
-  });
 
-   function counting() {    
-    if (document.getElementById('myCheck1').checked) {
-      correct++;
-      $("#correct").html("Correct answers: " + correct);
-      console.log("correct " + correct);
-    } else if (document.getElementById('myCheck2').checked) {
-      incorrect++;
-      $("#incorrect").html("Incorrect answers: " + incorrect);
-      console.log("incorrect " + incorrect);
-    }
-  };
+  function restart(){
+    number = 100;
+    run();
+    $('.container').show();
+    $('#answerScreen').hide();
+    $('input[type=radio]').prop('checked',false);
+  }
 
-  $("#donebtn").on("click", function () {
-    counting();
-    $("form").hide();
-    $(".results").show();
-    stop();
-  });
-
-  $("#startbtn").on("click", function () {
-    $("#startbtn").addClass("hidden");
-    $("form").show();
-    //a.setAttribute("hidden");
-    //$( "a" ).hide();
-    timer();
-  });
-
-  $("#restartbtn").on("click", function () {
+  //restartbtn
+  $("#restartbtn").on("click", function(){
     restart();
   });
-});
-
-
-
-
+  });
+  
